1cbc03296bec9638ce1e9e3dfdf1c8ba662f9e27
Aaron McUmber, Ph.D <aaron.mcumber@gmail.com>
2022-06-06 20:42:35 -0700

    feat: completed baseline model evaluation

--- capstone_tools/model_trainers.py
+++ capstone_tools/model_trainers.py
@@ -1,8 +1,21 @@
 from abc import ABC
+from typing import Dict
 
 import numpy as np
+from sklearn.metrics import accuracy_score
 
-from capstone_tools.data_splitting import DataSet, DataSetType
+from capstone_tools.data_splitting import DataLabelType
+
+
+class ModelProtocol(ABC):
+    def fit(self):
+        ...
+
+    def fit_predict(self):
+        ...
+
+    def predict(self):
+        ...
 
 
 class ModelTrainer(ABC):
@@ -11,7 +24,7 @@ class ModelTrainer(ABC):
     def __init__(self, *args, **kwargs):
         self.clf = ...
 
-    def train(self, epochs, data: DataSetType, labels: DataSetType):
+    def train(self, epochs, datasets: Dict[str, DataLabelType]):
         """Run main training loop"""
 
     def eval(self, sample):
@@ -20,4 +33,23 @@ class ModelTrainer(ABC):
 
 class SKLearnModelTrainer:
     def __init__(self, clf):
-        ...
+        self.clf = clf
+
+    def train(self, datasets: Dict[str, DataLabelType]):
+        accuracy = {}
+        train_data, train_labels = datasets["train"]
+        self.clf.fit(train_data, train_labels)
+        train_preds = self.clf.predict(train_data)
+        accuracy["train"] = accuracy_score(train_labels, train_preds)
+
+        # valid_data, valid_labels = datasets["validation"]
+        # valid_preds = self.clf.predict(valid_data)
+        # acc_train = accuracy_score(valid_labels, valid_preds)
+
+        test_data, test_labels = datasets["test"]
+        test_preds = self.clf.predict(test_data)
+        accuracy["test"] = accuracy_score(test_labels, test_preds)
+        return accuracy
+
+    def eval(self, sample):
+        return self.clf.predict(sample)
